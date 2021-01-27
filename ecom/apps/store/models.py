155029cf9b927ae7ffb8187917a542553f63181c
manos <manos-mark@hotmail.com>
2021-01-27 18:17:10 +0200

    beautify frontend

--- ecom/apps/store/models.py
+++ ecom/apps/store/models.py
@@ -9,7 +9,6 @@ class Store(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
-    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
 
     date_added = models.DateTimeField(auto_now_add=True)
     num_visits = models.IntegerField(default=0)
@@ -20,30 +19,8 @@ class Store(models.Model):
     def __str__(self):
         return self.name
 
-    def make_thumbnail(self, image, size=(300, 200)):
-        img = Image.open(image)
-        img.convert('RGB')
-        img.thumbnail(size)
-        
-        thumb_io = BytesIO()
-        img.save(thumb_io, 'JPEG', quality=85)
-
-        thumbnail = File(thumb_io, name=image.name)
-        return thumbnail
-
     def get_absolute_url(self):
         return '/%s/' % (self.slug)
-
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return self.thumbnail.url
-        else:
-            if self.image:
-                self.thumbnail = self.make_thumbnail(self.image) 
-                self.save()
-                return self.thumbnail.url
-            else:
-                return ''
 
 class Category(models.Model):
     store = models.ForeignKey(Store, related_name='categories', on_delete=models.CASCADE, blank=True, null=True)
@@ -52,7 +29,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=255)
     ordering = models.IntegerField(default=0)
     is_featured = models.BooleanField(default=False)
-    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -63,28 +40,6 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return '/%s/' % (self.slug)
-
-    def make_thumbnail(self, image, size=(300, 200)):
-        img = Image.open(image)
-        img.convert('RGB')
-        img.thumbnail(size)
-        
-        thumb_io = BytesIO()
-        img.save(thumb_io, 'JPEG', quality=85)
-
-        thumbnail = File(thumb_io, name=image.name)
-        return thumbnail
-
-    def get_thumbnail(self):
-        if self.thumbnail:
-            return self.thumbnail.url
-        else:
-            if self.image:
-                self.thumbnail = self.make_thumbnail(self.image) 
-                self.save()
-                return self.thumbnail.url
-            else:
-                return ''
 
 class Product(models.Model):
     store = models.ForeignKey(Store, related_name='products', on_delete=models.CASCADE, blank=True, null=True)
